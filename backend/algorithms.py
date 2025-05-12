61a6709a72555b7f007054c0bf0fb9cb82f4ce31
caiolamego <caio.lamego04@gmail.com>
2025-05-12 11:37:48 -0300

    implementa dijkstra
    Co-authored-by: MatheussBrant <matheus.s.brant@gmail.com>

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -1,3 +1,4 @@
+import heapq
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -13,3 +14,26 @@ class Graph:
     
     def get_neighbors(self, vertex):
         return self.vertices.get(vertex, {}).items()
+    
+    def dijkstra(graph, start):
+        distances = {v: float('infinity') for v in graph.vertices}
+        previous_nodes = {v: None for v in graph.vertices}
+        distances[start] = 0
+        
+        priority_queue = [(0, start)]
+        
+        while priority_queue:
+            current_dist, current_vertex = heapq.heappop(priority_queue)
+            
+            if current_dist > distances[current_vertex]:
+                continue
+            
+            for neighbor, weight in graph.get_neighbors(current_vertex):
+                distance = current_dist + weight
+                
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    previous_nodes[neighbor] = current_vertex
+                    heapq.heappush(priority_queue, (distance, neighbor))
+        
+        return previous_nodes, distances
