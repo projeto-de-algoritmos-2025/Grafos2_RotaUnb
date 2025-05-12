a9d3bc8269fd399c3b05baa5882fded0e1af2ac6
MatheussBrant <matheus.s.brant@gmail.com>
2025-05-12 11:50:59 -0300

    Adiciona rota principal e função para carregar dados do grafo a partir de arquivo JSON
    Co-Authored-By: caiolamego <caio.lamego04@gmail.com>

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+import json
+from flask import render_template
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(BASE_DIR, '../data/grafo.json')
 app = Flask(__name__, static_folder='../frontend', static_url_path='', template_folder='../frontend')
 CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+def load_campus_data():
+    with open(JSON_PATH, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
